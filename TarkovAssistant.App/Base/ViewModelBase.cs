388479659e3b478030ba7e29261d5a4c8b4ef4d1
Sergey Lubkov <sergeyl@devart.com>
2025-08-29 14:57:25 +0300

    added base level for commands and view models

---
+++ TarkovAssistant.App/Base/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace TarkovAssistant.App.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        protected ViewModelBase()
+        {
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler? handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        void IDisposable.Dispose()
+        {
+            OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+
+        }
+    }
+}
